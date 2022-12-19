24031d5d3f74380ec09d6a84d8d8ad9e633ec90a
webfaisalbd <web.faisal.bd@gmail.com>
2022-12-19 15:44:00 +0600

    set searchParams

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams, useSearchParams } from 'react-router-dom'
 
 const User = () => {
@@ -6,14 +6,35 @@ const User = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
 
-  return (
-    <div>
-        <h2>User</h2>
-        <h4>{searchParams.get('id')}</h4>
-        <h4>{searchParams.get('age')}</h4>
-        <h4>{searchParams.get('name')}</h4>
-    </div>
-  )
+    const [name, setName] = useState('');
+    const [id, setId] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchParams({ name: name, id: id })
+    }
+
+    return (
+        <div>
+            <h2>User</h2>
+
+            {/* set search query parameter   */}
+            <form onSubmit={handleSubmit}>
+                <input type="number" value={id} onChange={(e) => {
+                    setId(e.target.value)
+                }} placeholder='enter id' />
+                <input type="text" value={name} onChange={(e) => {
+                    setName(e.target.value)
+                }} placeholder='enter name' />
+                <button type="submit">Enter</button>
+            </form>
+
+            {/* get search query parameter   */}
+            <h4>{searchParams.get('id')}</h4>
+            <h4>{searchParams.get('age')}</h4>
+            <h4>{searchParams.get('name')}</h4>
+        </div>
+    )
 }
 
 export default User
