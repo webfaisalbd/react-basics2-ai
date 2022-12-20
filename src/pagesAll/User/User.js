2d13f9151b8fff17fffbf7e81501b0cea9deef29
webfaisalbd <web.faisal.bd@gmail.com>
2022-12-20 23:50:01 +0600

    set useSearchParams

--- src/pagesAll/User/User.js
+++ src/pagesAll/User/User.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import { useSearchParams } from 'react-router-dom'
+import React, {useState} from 'react'
+import { useSearchParams } from 'react-router-dom';
 
 const User = () => {
-const [searchQuery, setSearchQuery] = useSearchParams();
+
+    const [searchQuery, setSearchQuery] = useSearchParams();
+
+    const [user, setUser] = useState({name: '', age: ''});
+    
+    const handleChange = (e) => {
+        setUser({...user, [e.target.name] : e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user);
+        setSearchQuery({name: user.name, age: user.age});
+    }
     return (
-    <div>
-        <h2>User</h2>
+        <div>
+            <h2>User</h2>
 
-        <h3>{searchQuery.get('id')}</h3>
-        <h4>{searchQuery.get('name')}</h4>
+            <form onSubmit={handleSubmit}>
+                <input name='name' value={user.name} onChange={handleChange} type="text" placeholder='enter name' />
+                <input name='age' value={user.age} onChange={handleChange} type="number" placeholder='enter age' />
+                <button type='submit'>Enter</button>
+            </form>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default User
