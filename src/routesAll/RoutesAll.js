23e095c91afb05c06dc34b185129d9017f9b1fe4
webfaisalbd <web.faisal.bd@gmail.com>
2022-12-21 10:48:07 +0600

    protected route practice

--- src/routesAll/RoutesAll.js
+++ src/routesAll/RoutesAll.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // pages 
@@ -9,16 +9,22 @@ import About from '../pagesAll/About/About'
 import Error from '../pagesAll/Error/Error'
 import Navbar from '../layout/Navbar'
 import User from '../pagesAll/User/User'
+import Protected from '../layout/Protected'
 
 const RoutesAll = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return <BrowserRouter>
         <Navbar />
+        <button onClick={()=> setIsLoggedIn(!isLoggedIn)}>{isLoggedIn ? 'Log Out' : 'Log In'}</button>
         <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/blogs' element={<Blogs />}></Route>
             <Route path='/blogs/:title' element={<Blog />}></Route>
             <Route path='/about' element={<About />}></Route>
-            <Route path='/user' element={<User />}></Route>
+            <Route path='/user' element={ <Protected isLoggedIn={isLoggedIn}>
+                <User />
+            </Protected> }></Route>
             <Route path='*' element={<Error />}></Route>
         </Routes>
     </BrowserRouter>
